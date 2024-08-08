6dce105e77454a221a85ae64401bcfdfb0644865
egeecevit <egeecevit@gmail.com>
2024-08-08 17:36:24 +0300

    pure pursuit works

--- src/bicycle_model/bicycle_model/bicycle_control.py
+++ src/bicycle_model/bicycle_model/bicycle_control.py
@@ -15,68 +15,132 @@ class BicycleControl(Node):
         self.bicyc_length = 1
         self.v = 10
         self.gamma_dot = 0
-        self.theta = 0
-        self.dt = 0.01
-        self.x_t = 7
-        self.y_t = 7
+        self.theta = 0 * math.pi / 180
+        self.dt = 0.005
+        self.x_t = 0
+        self.y_t = -2
+        self.Kdd = 0.03
+        self.ld = self.Kdd * self.v
+        self.x = list(np.arange(0, 100.1, 0.1))
+        self.y = []
+        for num in self.x:
+            self.y.append(math.sin(num*2.0*math.pi/40.0))
+        
+        self.x_tp = 0
+        self.y_tp = 0
+        self.alpha = 0
+        self.start = True
+        self.x_in_circle = []
+        self.y_in_circle = []
+        self.circle = False
 
 
-        self.declare_parameter("steering_angle",30)
-
-        self.timer = self.create_timer(0.01,self.timer_callback)
+        self.timer = self.create_timer(0.005,self.timer_callback)
 
         # Initialize plot
         self.fig, self.ax = plt.subplots()
-        self.x_data, self.y_data = [], []
-        self.line, = self.ax.plot(self.x_data, self.y_data, 'r-')
-
-        #plt.axhline(y=15, color = 'b')
-        x = np.linspace(0,30,100)
-        y = -x + 15
-        plt.plot(x, y, '-g')
+        self.x_data, self.y_data = [self.x_t], [self.y_t]
+        self.line, = self.ax.plot(self.x_data, self.y_data, 'r-', linewidth=2)
         plt.ion()
         plt.show()
+    
+    def data_in_circle(self, x, x_t, y, y_t):
+        for i in range(len(x)-1):
+            distance = math.sqrt((x[i] - x_t)**2 + (y[i] - y_t)**2)
+            if distance <= self.ld:
+                self.x_in_circle.append(x[i])
+                self.y_in_circle.append(y[i])
+            if len(self.x_in_circle) > 2 and len(self.y_in_circle) > 2:
+                self.x_in_circle.pop(0)
+                self.y_in_circle.pop(0)
+
+    
+    def find_largest(self, x, xtp, y, ytp):
+        largestx = None
+        largesty = None
+
+        for i in range(len(x)-1):
+            if x[i] < xtp:
+                if largestx is None or x[i] > largestx:
+                    largestx = x[i]
+            if y[i] < ytp:
+                if largesty is None or y[i] > largesty:
+                    largesty = y[i]
+        
+        return largestx, largesty
+    
+    def find_smallest(self, x, x_p, y, y_p):
+        smallestx = None
+        smallesty = None
+
+        for i in range(len(x)-1):
+            if x[i] > x_p:
+                if smallestx is None or x[i] < smallestx:
+                    smallestx = x[i]
+                    smallesty = y[i]
+
+        return smallestx, smallesty
+    
+    def update_circle(self, circle):
+        if circle:
+            circle.remove()
+        circle = plt.Circle( (self.x_t, self.y_t), self.ld, fill = False )
+        self.ax.add_artist(circle)
+        plt.draw()
+        return circle
+
 
     def timer_callback(self):
-        # steering_ang = self.get_parameter("steering_angle").value
+        #----------------------Pure Pursuit------------------------------------------------------
+        #self.ld = self.Kdd * self.v
+        self.data_in_circle(self.x, self.x_t, self.y, self.y_t)
+        # print(f'x_in_circle: {self.x_in_circle}, y_in_circle: {self.y_in_circle}')
+        self.circle = self.update_circle(self.circle)
 
-        # steering_ang = steering_ang * math.pi / 180
+        x_p, y_p = self.find_smallest(self.x, self.x_tp, self.y, self.y_tp)
+        dist_p = math.sqrt((self.x_t)**2 + (self.y_t)**2)
+        dist_tp = math.sqrt((self.x_tp)**2 + (self.y_tp)**2)
+        dist_max = math.sqrt(self.x[-2]**2 + self.y[-2]**2)
 
-        # line equation ax+by+c=0
-        a = 1
-        b = 1
-        c = -15
+        if dist_p > dist_max:
+            print("I am here")
+            self.alpha = np.arctan2((self.y[-2] - self.y_t), (self.x[-2] - self.x_t)) - self.theta
+            
+        elif len(self.x_in_circle) == 0:
+            self.x_tp, self.y_tp = self.find_smallest(self.x, self.x_t, self.y, self.y_t)
+            self.alpha = np.arctan2((self.y_tp - self.y_t), (self.x_tp - self.x_t)) - self.theta
 
-        Kd = 0.5
-        Kh = 1
+        elif len(self.x_in_circle) > 0:
+            self.x_tp, self.y_tp = max(self.x_in_circle), max(self.y_in_circle)
+            self.alpha = np.arctan2((self.y_tp - self.y_t), (self.x_tp - self.x_t)) - self.theta
+            if dist_p > dist_tp:
+                self.x_tp, self.y_tp = self.find_smallest(self.x, self.x_t, self.y, self.y_t)
+                self.alpha = np.arctan2((self.y_tp - self.y_t), (self.x_tp - self.x_t)) - self.theta
+        print(f'xtp: {self.x_tp}, ytp: {self.y_tp}\n x_in_circle: {self.x_in_circle}, y_in_circle: {self.y_in_circle}\n x_t: {self.x_t}, y_t: {self.y_t}')
 
-        theta_s = math.atan2(-a, b) #radians
-        line_array = np.array([a, b, c])
-        position_array = np.array([self.x_t, self.y_t, 1])
-        d = np.dot(line_array, position_array) / math.sqrt(a**2 + b**2)
-        print(f'distance from the line : {d}\n')
-        
-        steering_ang = -Kd * d + Kh * (theta_s - self.theta)
-        print(f'theta: {self.theta}\n')
-        print(f'steering angle : {steering_ang}')
+        # elif dist1 < dist2:
+        #     print("burdayım")
+        #     self.x_tp, self.y_tp = max(self.x_in_circle), max(self.y_in_circle)
+        #     self.alpha = np.arctan2((self.y_tp - self.y_t), (self.x_tp - self.x_t)) - self.theta
+        #     print(f'xtp: {self.x_tp}, ytp: {self.y_tp}\n x_in_circle: {self.x_in_circle}, y_in_circle: {self.y_in_circle}\n x_t: {self.x_t}, y_t: {self.y_t}')
+            # else:
+            #     print(f'xtp: {self.x_tp}, ytp: {self.y_tp}\n x_in_circle: {self.x_in_circle}, y_in_circle: {self.y_in_circle}\n x_t: {self.x_t}, y_t: {self.y_t}')
+            #     self.x_tp, self.y_tp = max(self.x_in_circle), max(self.y_in_circle)
+            #     self.alpha = np.arctan2((self.y_tp - self.y_t), (self.x_tp - self.x_t)) - self.theta
 
-        #steering_ang = -14 * math.pi /180
-        # Rear wheel is the reference Bicycle Kinematic
+
+        steering_ang = math.atan((2 * self.bicyc_length * math.sin(self.alpha)) / self.ld)
+        #----------------------------------------------------------------------------------------------
+
+        # ----------------Rear wheel is the reference Bicycle Kinematic----------------------------------
         x_dot = self.v * math.cos(self.theta)
         y_dot = self.v * math.sin(self.theta)
         self.theta_dot = self.v * math.tan(steering_ang) / self.bicyc_length
-        self.theta += self.theta_dot * self.dt
-
-
 
         self.x_t += x_dot * self.dt
         self.y_t += y_dot * self.dt
-
-        """------------------------------------------------------------------------------------------------------------------"""
-
-        # self.get_logger().info(f'x_dot: {x_dot}, y_dot: {y_dot}, theta_dot: {self.theta_dot}, gamma_dot: {self.gamma_dot}\n')
-        # self.get_logger().info(f'x: {self.x_t} y: {self.y_t}\n')
-        
+        self.theta += self.theta_dot * self.dt
+        #------------------------------------------------------------------------------------------------------------------        
 
 
         # Update plot
@@ -86,8 +150,11 @@ class BicycleControl(Node):
         self.line.set_ydata(self.y_data)
         self.ax.relim()
         self.ax.autoscale_view()
+        plt.plot(self.x_tp, self.y_tp, 'ro')
+        plt.plot(self.x, self.y, 'b', linewidth=0.5)
         plt.draw()
         plt.pause(0.01)
+
 
 def main(args=None):
     rclpy.init(args=args)
